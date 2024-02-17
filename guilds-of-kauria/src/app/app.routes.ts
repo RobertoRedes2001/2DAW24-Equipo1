00ecf415e81656aab8479be4189c055ae506365c
RobertoRedes2001 <romaav@floridauniversitaria.es>
2024-02-17 13:07:47 +0100

    re-hecho proyecto angular

--- guilds-of-kauria/src/app/app.routes.ts
+++ guilds-of-kauria/src/app/app.routes.ts
@@ -1,16 +1,17 @@
 import { Routes } from '@angular/router';
-import { MainEnComponent } from './views/main-en/main-en.component';
 import { MainEsComponent } from './views/main-es/main-es.component';
-import { GameplayEnComponent } from './views/gameplay-en/gameplay-en.component';
-import { GameplayEsComponent } from './views/gameplay-es/gameplay-es.component';
+import { MainEnComponent } from './views/main-en/main-en.component';
 import { CardsEsComponent } from './views/cards-es/cards-es.component';
 import { CardsEnComponent } from './views/cards-en/cards-en.component';
-import { NewsEsComponent } from './views/news-es/news-es.component';
+import { GameplayEsComponent } from './views/gameplay-es/gameplay-es.component';
+import { GameplayEnComponent } from './views/gameplay-en/gameplay-en.component';
 import { NewsEnComponent } from './views/news-en/news-en.component';
+import { NewsEsComponent } from './views/news-es/news-es.component';
 
 
 export const routes: Routes = [
     { path: 'es', component: MainEsComponent },
+    { path: '', pathMatch: 'full', redirectTo: 'es' },
     { path: 'en', component: MainEnComponent },
     { path: 'es/game', component: GameplayEsComponent },
     { path: 'en/game', component: GameplayEnComponent },
