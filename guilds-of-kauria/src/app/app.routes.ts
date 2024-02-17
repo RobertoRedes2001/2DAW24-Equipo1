9b5631909ab01c2d531215ad6b6fcefd0e62294a
RobertoRedes2001 <romaav@floridauniversitaria.es>
2024-02-17 19:32:19 +0100

    solventando rutas

--- guilds-of-kauria/src/app/app.routes.ts
+++ guilds-of-kauria/src/app/app.routes.ts
@@ -1,8 +1,14 @@
 import { Routes } from '@angular/router';
 import { MainComponent } from './views/main/main.component';
+import { GameComponent } from './views/game/game.component';
+import { CardsComponent } from './views/cards/cards.component';
+import { NewsComponent } from './views/news/news.component';
 
 
 export const routes: Routes = [
     { path: '', component: MainComponent },
     { path: '', pathMatch: 'full', redirectTo: '' },
+    { path: 'cards', component: CardsComponent },
+    { path: 'game', component: GameComponent },
+    { path: 'news', component: NewsComponent },
 ];
