00ac1d631cf1381aac90de16ba714e268d356139
RobertoRedes2001 <romaav@floridauniversitaria.es>
2024-02-18 11:21:20 +0100

    modificaciones

--- guilds-of-kauria/src/app/views/game/game.component.ts
+++ guilds-of-kauria/src/app/views/game/game.component.ts
@@ -1,12 +1,24 @@
+import { NgClass } from '@angular/common';
 import { Component } from '@angular/core';
+import { TranslateModule } from '@ngx-translate/core';
 
 @Component({
   selector: 'app-game',
   standalone: true,
-  imports: [],
+  imports: [NgClass,TranslateModule],
   templateUrl: './game.component.html',
   styleUrl: './game.component.css'
 })
 export class GameComponent {
+
+  public changeTextTitle : string = "Ejemplo";
+  public changeTextBody : string = "Lorem";
+  public learn : boolean = true;
+
+  public onChangeText(title : string, body : string){
+    this.changeTextTitle = title;
+    this.changeTextBody = body;
+    this.learn = true;
+  }
 
 }
