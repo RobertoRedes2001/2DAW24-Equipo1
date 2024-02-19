c0b87887cebca95952238d966a68ad2a676a7ace
Joan2k2 <joanruizmartinez@gmail.com>
2024-02-19 23:28:33 +0100

    try to do card

--- guilds-of-kauria/src/app/views/cards/cards.component.ts
+++ guilds-of-kauria/src/app/views/cards/cards.component.ts
@@ -1,12 +1,15 @@
 import { Component } from '@angular/core';
+import { CardComponent } from '../../components/card/card.component';
 
 @Component({
   selector: 'app-cards',
   standalone: true,
-  imports: [],
+  imports: [CardComponent],
   templateUrl: './cards.component.html',
   styleUrl: './cards.component.css'
 })
 export class CardsComponent {
+
+ public items:number[]=[1,2,3];
 
 }
