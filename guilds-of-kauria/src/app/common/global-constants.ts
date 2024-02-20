ca5f04d7e1f386119ca69eca12eed23d0bb060aa
Joan2k2 <joanruizmartinez@gmail.com>
2024-02-20 09:57:05 +0100

    cards all

--- guilds-of-kauria/src/app/common/global-constants.ts
+++ guilds-of-kauria/src/app/common/global-constants.ts
@@ -1,7 +1,7 @@
 export class GlobalConstants {
 
 
-    
+    public static cardsVisibility: boolean = false;
     public static appURLApi: string = "example";  
     public static currentLang: string = "es";
 
--- guilds-of-kauria/src/app/views/cards/cards.component.ts
+++ guilds-of-kauria/src/app/views/cards/cards.component.ts
@@ -1,15 +1,28 @@
 import { Component } from '@angular/core';
 import { CardComponent } from '../../components/card/card.component';
+import { NgStyle } from '@angular/common';
+import { GlobalConstants } from '../../common/global-constants';
 
 @Component({
   selector: 'app-cards',
   standalone: true,
-  imports: [CardComponent],
+  imports: [CardComponent,NgStyle],
   templateUrl: './cards.component.html',
   styleUrl: './cards.component.css'
 })
 export class CardsComponent {
+  cardVisibility: boolean = GlobalConstants.cardsVisibility;
+ public items:number[]=[1,2,3,4,5,6];
 
- public items:number[]=[1,2,3];
+  public cardClicked(){
+
+    if("llamada a la api hay resultados"){
+
+      GlobalConstants.cardsVisibility=true;
+      this.cardVisibility=true;
+    }
+
+  }
+
 
 }
