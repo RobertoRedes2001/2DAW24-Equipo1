c9da9d2d811c551cb62e060f66703eb5b103c08b
Joan2k2 <joanruizmartinez@gmail.com>
2024-02-20 18:13:58 +0100

    try to finish cards(structure)

--- guilds-of-kauria/src/app/components/card/card.component.ts
+++ guilds-of-kauria/src/app/components/card/card.component.ts
@@ -5,13 +5,11 @@ import { Component, Input } from '@angular/core';
   standalone: true,
   imports: [],
   templateUrl: './card.component.html',
-  styleUrl: './card.component.css'
+  styleUrl: './card.component.css',
 })
 export class CardComponent {
-
-  @Input() title:string="Michi";
-  @Input() img:string="https://external-content.duckduckgo.com/iu/?u=http%3A%2F%2Flacocinadebender.com%2Fwp-content%2Fuploads%2F2016%2F07%2Fvideo-gatos-gordos-graciosos.jpg%3Fis-pending-load%3D1&f=1&nofb=1&ipt=1ff8ed8ed020b04493e71584053968b1d3a5f5499497c18774d4769c2c302d64&ipo=images";
-  @Input() littleDescription:string="The legendary Michi";
-
-
+  @Input() title: string = 'Michi';
+  @Input() img: string =
+    'https://external-content.duckduckgo.com/iu/?u=http%3A%2F%2Flacocinadebender.com%2Fwp-content%2Fuploads%2F2016%2F07%2Fvideo-gatos-gordos-graciosos.jpg%3Fis-pending-load%3D1&f=1&nofb=1&ipt=1ff8ed8ed020b04493e71584053968b1d3a5f5499497c18774d4769c2c302d64&ipo=images';
+  @Input() littleDescription: string = 'The legendary Michi';
 }
--- guilds-of-kauria/src/app/views/cards/cards.component.ts
+++ guilds-of-kauria/src/app/views/cards/cards.component.ts
@@ -2,43 +2,79 @@ import { Component } from '@angular/core';
 import { CardComponent } from '../../components/card/card.component';
 import { NgStyle } from '@angular/common';
 import { GlobalConstants } from '../../common/global-constants';
+import { ReactiveFormsModule, FormControl } from '@angular/forms';
 
 
 @Component({
   selector: 'app-cards',
   standalone: true,
-  imports: [CardComponent,NgStyle],
+  imports: [CardComponent,NgStyle,ReactiveFormsModule],
   templateUrl: './cards.component.html',
   styleUrl: './cards.component.css'
 })
 export class CardsComponent {
+
+
+
   cardVisibility: boolean = false;
-  public items:number[]=[1,2,3,4,5,6];
-  public cardType: string = 'null'; // Variable para almacenar el valor seleccionado del select
+  items:number[]=[1,2,3,4,5,6];
+  cardType: string = ''; // Variable para almacenar el valor seleccionado del select
+  cardEdition:string="";
+  cardStatus:string="";
+  cardTypeStats:string="";
+  principalInputName = new FormControl('');
+  secondaryInputName = new FormControl('');
+  amountStats = new FormControl('');
+  amountMana = new FormControl('');
 
-onChangeCardEdition($event: Event) {
-throw new Error('Method not implemented.');
+
+
+onChangeCardEdition(event: any) {
+this.cardEdition=event.target.value;
 }
 
-onChangeCardStatus($event: Event) {
-throw new Error('Method not implemented.');
+onChangeCardStatus(event: any) {
+  this.cardStatus=event.target.value;
 }
 
-onChangeCardStats($event: Event) {
-throw new Error('Method not implemented.');
+onChangeCardStats(event: any) {
+  this.cardTypeStats=event.target.value;
 }
 
 
 
  onChangeCardType(event: any) {
-  const valorSeleccionado = event.target.value;
-  console.log(valorSeleccionado); // Muestra el valor seleccionado en la consola
+  this.cardType=event.target.value;
 }
 
-  public cardClicked(){
+  public buttonFilterClicked(){
     //llamada a la api hay resultados
     if(this.cardVisibility===false){
       this.cardVisibility=true;
+  console.log('Tipo de carta seleccionado:', this.cardType);
+  console.log('Edición de la carta seleccionada:', this.cardEdition);
+  console.log('Estado de la carta seleccionado:', this.cardStatus);
+  console.log('Estadísticas del tipo de carta seleccionado:', this.cardTypeStats);
+  console.log('Valor del primer input:', this.principalInputName.value);
+  console.log('Valor del segundo input:', this.secondaryInputName.value);
+  console.log('Cantidad de estadísticas:', this.amountStats.value);
+  console.log('Cantidad de mana:', this.amountMana.value);
+    }
+
+  }
+
+  public firstButtonClicked(){
+    //llamada a la api hay resultados
+    if(this.cardVisibility===false){
+      this.cardVisibility=true;
+  console.log('Tipo de carta seleccionado:', this.cardType);
+  console.log('Edición de la carta seleccionada:', this.cardEdition);
+  console.log('Estado de la carta seleccionado:', this.cardStatus);
+  console.log('Estadísticas del tipo de carta seleccionado:', this.cardTypeStats);
+  console.log('Valor del primer input:', this.principalInputName.value);
+  console.log('Valor del segundo input:', this.secondaryInputName.value);
+  console.log('Cantidad de estadísticas:', this.amountStats.value);
+  console.log('Cantidad de mana:', this.amountMana.value);
     }
 
   }
