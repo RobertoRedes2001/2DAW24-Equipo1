6030030c117593f0809492ac9fdde1e9f7be7a09
Joan2k2 <joanruizmartinez@gmail.com>
2024-02-20 13:14:30 +0100

    sections velue

--- guilds-of-kauria/src/app/views/cards/cards.component.ts
+++ guilds-of-kauria/src/app/views/cards/cards.component.ts
@@ -3,6 +3,7 @@ import { CardComponent } from '../../components/card/card.component';
 import { NgStyle } from '@angular/common';
 import { GlobalConstants } from '../../common/global-constants';
 
+
 @Component({
   selector: 'app-cards',
   standalone: true,
@@ -11,14 +12,32 @@ import { GlobalConstants } from '../../common/global-constants';
   styleUrl: './cards.component.css'
 })
 export class CardsComponent {
-  cardVisibility: boolean = GlobalConstants.cardsVisibility;
- public items:number[]=[1,2,3,4,5,6];
+  cardVisibility: boolean = false;
+  public items:number[]=[1,2,3,4,5,6];
+  public cardType: string = 'null'; // Variable para almacenar el valor seleccionado del select
+
+onChangeCardEdition($event: Event) {
+throw new Error('Method not implemented.');
+}
+
+onChangeCardStatus($event: Event) {
+throw new Error('Method not implemented.');
+}
+
+onChangeCardStats($event: Event) {
+throw new Error('Method not implemented.');
+}
+
+
+
+ onChangeCardType(event: any) {
+  const valorSeleccionado = event.target.value;
+  console.log(valorSeleccionado); // Muestra el valor seleccionado en la consola
+}
 
   public cardClicked(){
-
-    if("llamada a la api hay resultados"){
-
-      GlobalConstants.cardsVisibility=true;
+    //llamada a la api hay resultados
+    if(this.cardVisibility===false){
       this.cardVisibility=true;
     }
 
